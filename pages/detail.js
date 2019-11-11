a803ee3f33edd31beab68ddf114dd37271e97666
huanghui <815189517@qq.com>
2019-11-11 10:13:59 +0800

    'markdown'

--- pages/detail.js
+++ pages/detail.js
@@ -1,49 +1,104 @@
 import React from 'react'
 import Head from 'next/head'
-import { Row,Col,Icon,Breadcrumb} from 'antd'
+import { Row, Col, Icon, Breadcrumb, Affix } from 'antd'
 import Header from '../components/Header'
 import Author from '../components/Author'
 import Advert from '../components/Advert'
 import Footer from '../components/Footer'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
 import '../static/style/components/detail.css'
 
-const Detail = () => (
-  <>
-    <Head>
-      <title>Detail</title>
-    </Head>
-    <Header />
-    <Row className='comm-main' type='flex' justify='center'>
-      <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
-        <div className='bread-div'>
-          <Breadcrumb>
-            <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
-            <Breadcrumb.Item><a href="/list">视频列表</a></Breadcrumb.Item>
-            <Breadcrumb.Item>视频详情</Breadcrumb.Item>
-          </Breadcrumb>
-        </div>
-    
-        <div className='detail-title'>从0开始构建Webpack</div>
+const Detail = () => {
+  let markdown = '# P01:课程介绍和环境搭建\n' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+    '**这是加粗的文字**\n\n' +
+    '*这是倾斜的文字*`\n\n' +
+    '***这是斜体加粗的文字***\n\n' +
+    '~~这是加删除线的文字~~ \n\n' +
+    '\`console.log(111)\` \n\n' +
+    '# p02:来个Hello World 初始Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n' +
+    '***\n\n\n' +
+    '# p03:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p04:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '#5 p05:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p06:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p07:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '``` var a=11; ```'
+  return (
+    <>
+      <Head>
+        <title>Detail</title>
+      </Head>
+      <Header />
+      <Row className='comm-main' type='flex' justify='center'>
+        <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
+          <div className='bread-div'>
+            <Breadcrumb>
+              <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+              <Breadcrumb.Item><a href="/list">视频列表</a></Breadcrumb.Item>
+              <Breadcrumb.Item>视频详情</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
 
-        <div className='list-icon'>
-          <span><Icon type="calendar"/> 2019-11-06</span>
-          <span><Icon type="folder" /> 2019-11-06</span>
-          <span><Icon type="fire" /> 2019-11-06</span>
-        </div>
+          <div className='detail-title'>从0开始构建Webpack</div>
 
-        <div className='detail-content'>
-          详情
-        </div>
+          <div className='list-icon'>
+            <span><Icon type="calendar" /> 2019-11-06</span>
+            <span><Icon type="folder" /> 2019-11-06</span>
+            <span><Icon type="fire" /> 2019-11-06</span>
+          </div>
 
-      </Col>
+          <div className='detail-content'>
+            <ReactMarkdown
+              source={markdown}
+              escapeHtml={false}
+            />
+          </div>
 
-      <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
-        <Author />
-        <Advert />
-      </Col>
-      <Footer />
-    </Row>
-  </>
-)
+        </Col>
+
+        <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+          <Affix>
+            <div className='detail-nav'>
+              <div className="nav-title">文章目录</div>
+              <MarkNav
+                className='article-menu'
+                source={markdown}
+                ordered={false}
+              />
+            </div>
+          </Affix>
+        </Col>
+        <Footer />
+      </Row>
+    </>
+
+  )
+}
+
+
 
 export default Detail
